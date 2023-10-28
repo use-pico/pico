499f989fdaab8230de97baa3f43c3f8f945ad0b2
Marek Hanzal <marek.hanzal@x32.cz>
2023-10-29 00:51:48 +0200

    Improved JobAsync

--- packages/job/src/ui/JobAsync.tsx
+++ packages/job/src/ui/JobAsync.tsx
@@ -70,7 +70,7 @@ export const JobAsync = <
                     inline
                     job={job}
                     progressProps={{
-                        size: "xs",
+                        size: "sm",
                     }}
                 />}
             </Stack>
