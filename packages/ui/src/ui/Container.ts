36f5dc0f6d713a7242811528ae24980862c068e0
Marek Hanzal <marek.hanzal@x32.cz>
2023-11-17 17:32:26 +0100

    Version bump

--- packages/ui/src/ui/Container.ts
+++ packages/ui/src/ui/Container.ts
@@ -1,3 +1,8 @@
 "use client";
 
+import {type ComponentProps} from "react";
+
 export {Container} from "@mantine/core";
+export namespace Container {
+    export type Props = ComponentProps<typeof Container>;
+}
