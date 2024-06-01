c0e9ab808aac53755929437735b9451247cc7530
Marek Hanzal <marek.hanzal@x32.cz>
2024-06-01 02:29:28 +0200

    Docs

--- packages/client/src/i18n/LocaleRedirect.tsx
+++ packages/client/src/i18n/LocaleRedirect.tsx
@@ -1,29 +1,46 @@
 "use client";
 
 import {type localeOf} from "@use-pico/common";
-import {useRouter} from "next/navigation";
-import {type FC} from "react";
-import {DetectLocale} from "./DetectLocale";
+import {useRouter}     from "next/navigation";
+import {type FC}       from "react";
+import {DetectLocale}  from "./DetectLocale";
 
+/**
+ * When this component is used, it immediately redirects to the target URL with the detected locale.
+ *
+ * @group i18n
+ */
 export namespace LocaleRedirect {
-	export interface Props {
-		target?: string;
-		locale: localeOf.Props;
-		detectLocaleProps?: Omit<DetectLocale.Props, "locale" | "callback">;
-	}
+    /**
+     * Props for `LocaleRedirect`.
+     */
+    export interface Props {
+        /**
+         * Target URL.
+         */
+        target?: string;
+        /**
+         * Locale to redirect to.
+         */
+        locale: localeOf.Props;
+        /**
+         * Props for `DetectLocale`.
+         */
+        detectLocaleProps?: Omit<DetectLocale.Props, "locale" | "callback">;
+    }
 }
 
 export const LocaleRedirect: FC<LocaleRedirect.Props> = (
-	{
-		target,
-		locale,
-		detectLocaleProps,
-	}) => {
-	const router = useRouter();
+    {
+        target,
+        locale,
+        detectLocaleProps,
+    }) => {
+    const router = useRouter();
 
-	return <DetectLocale
-		locale={locale}
-		callback={({locale}) => router.push(target ? `/${locale}/${target}` : `/${locale}`)}
-		{...detectLocaleProps}
-	/>;
+    return <DetectLocale
+        locale={locale}
+        callback={({locale}) => router.push(target ? `/${locale}/${target}` : `/${locale}`)}
+        {...detectLocaleProps}
+    />;
 };
--- packages/common/src/i18n/localeOf.ts
+++ packages/common/src/i18n/localeOf.ts
@@ -1,28 +1,33 @@
 import {preferredLocale} from "preferred-locale";
 
-export namespace localeOf {
-	export interface Props {
-		/**
-		 * Available locales in your app.
-		 */
-		available: string[];
-		/**
-		 * Fallback locale.
-		 */
-		fallback: string;
-	}
-}
-
 /**
  * Extract preferred locale from the available locales and fallback locale.
+ *
+ * @group i18n
  */
+export namespace localeOf {
+    /**
+     * Props for `localeOf`.
+     */
+    export interface Props {
+        /**
+         * Available locales in your app.
+         */
+        available: string[];
+        /**
+         * Fallback locale.
+         */
+        fallback: string;
+    }
+}
+
 export const localeOf = (
-	{
-		available,
-		fallback
-	}: localeOf.Props
+    {
+        available,
+        fallback
+    }: localeOf.Props
 ) => {
-	return preferredLocale(fallback, available, {
-		languageOnly: true,
-	});
+    return preferredLocale(fallback, available, {
+        languageOnly: true,
+    });
 };
