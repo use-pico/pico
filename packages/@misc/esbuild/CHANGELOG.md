9c2f9137c9b0029ca0fd319a0a2fc9546f75422d
Marek Hanzal <marek.hanzal@icloud.com>
2024-11-26 10:44:48 +0100

    Updated deps

--- packages/@misc/esbuild/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# @use-pico/esbuild
-
-## 3.0.1
-
-### Patch Changes
-
-- 83a3147: Initial release of 3.0
--- packages/@misc/esbuild/src/build.ts
+++
@@ -1,4 +0,0 @@
-import {build as coolBuild, type BuildOptions} from "esbuild";
-import {withEsbuild} from "./withEsbuild";
-
-export const build = async (config?: BuildOptions) => coolBuild(withEsbuild(config));
