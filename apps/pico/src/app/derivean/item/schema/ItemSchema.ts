c1767d76ea075a9aa67b8713a181d820dc31a4c8
Marek Hanzal <marek.hanzal@icloud.com>
2024-12-25 15:11:12 +0100

    Wut?

--- apps/pico/src/app/derivean/item/schema/ItemSchema.ts
+++ apps/pico/src/app/derivean/item/schema/ItemSchema.ts
@@ -1,10 +1,11 @@
 import { IdentitySchema } from "@use-pico/common";
 import { z } from "zod";
+import { ItemKindSchema } from "~/app/derivean/item/schema/ItemKindSchema";
 
 export const ItemSchema = IdentitySchema.merge(
 	z.object({
-		name: z.string(),
-		kind: z.enum(["building", "resource"]),
+		name: z.string().min(1),
+		kind: ItemKindSchema,
 	}),
 );
 
