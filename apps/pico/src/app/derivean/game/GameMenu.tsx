1b75ba48cdd13c7a6f2c92440827c25bf4cbd881
Marek Hanzal <marek.hanzal@icloud.com>
2025-01-01 15:29:40 +0100

    Updated game menu to point to the root

--- apps/pico/src/app/derivean/game/GameMenu.tsx
+++ apps/pico/src/app/derivean/game/GameMenu.tsx
@@ -1,6 +1,6 @@
+import { useParams } from "@tanstack/react-router";
 import { Menu, MenuLink, Tx } from "@use-pico/client";
 import type { FC } from "react";
-import { BlueprintIcon } from "~/app/derivean/icon/BlueprintIcon";
 
 export namespace GameMenu {
 	export interface Props extends Menu.Props {
@@ -9,10 +9,15 @@ export namespace GameMenu {
 }
 
 export const GameMenu: FC<GameMenu.Props> = (props) => {
+	const { locale } = useParams({ from: "/$locale" });
+
 	return (
 		<Menu {...props}>
-			<MenuLink icon={BlueprintIcon}>
-				<Tx label={"[Dummy] (menu)"} />
+			<MenuLink
+				to={"/$locale/apps/derivean/root"}
+				params={{ locale }}
+			>
+				<Tx label={"Root site (menu)"} />
 			</MenuLink>
 		</Menu>
 	);
