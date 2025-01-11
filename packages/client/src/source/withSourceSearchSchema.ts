79483607f72acead7b72b7ac422ad5055bae1b1e
Marek Hanzal <marek.hanzal@icloud.com>
2025-01-11 13:36:33 +0100

    Updated search schema

--- packages/client/src/source/withSourceSearchSchema.ts
+++ packages/client/src/source/withSourceSearchSchema.ts
@@ -3,19 +3,24 @@ import { CursorSchema, type FilterSchema } from "@use-pico/common";
 import { z } from "zod";
 
 export namespace withSourceSearchSchema {
+	export interface Opts {
+		size?: number;
+	}
+
 	export interface Props<TFilterSchema extends FilterSchema> {
 		filter: TFilterSchema;
 	}
 }
 
-export const withSourceSearchSchema = <TFilterSchema extends FilterSchema>({
-	filter,
-}: withSourceSearchSchema.Props<TFilterSchema>) => {
+export const withSourceSearchSchema = <TFilterSchema extends FilterSchema>(
+	{ filter }: withSourceSearchSchema.Props<TFilterSchema>,
+	{ size = 15 }: withSourceSearchSchema.Opts = { size: 15 },
+) => {
 	return z.object({
 		filter: fallback(filter.optional(), undefined),
-		cursor: fallback(CursorSchema, { page: 0, size: 15 }).default({
+		cursor: fallback(CursorSchema, { page: 0, size }).default({
 			page: 0,
-			size: 15,
+			size,
 		}),
 		sort: fallback(z.any().optional(), undefined),
 		selection: fallback(z.array(z.string()), []).default([]),
