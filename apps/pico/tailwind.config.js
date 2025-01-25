b77928812d1f8e3ae7c96cbc8278d107edd48f78
Marek Hanzal <marek.hanzal@icloud.com>
2025-01-25 12:43:30 +0100

    Experiment

--- apps/pico/tailwind.config.js
+++ apps/pico/tailwind.config.js
@@ -1,9 +1,8 @@
 import { addDynamicIconSelectors } from "@iconify/tailwind";
-import type { Config } from "tailwindcss";
 
 export default {
 	important: true,
 	darkMode: ["class"],
 	content: ["./src/**/*.{html,js,jsx,ts,tsx}", "../../packages/**/*.{ts,tsx}"],
 	plugins: [addDynamicIconSelectors()],
-} satisfies Config;
+};
