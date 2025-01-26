218d76972b3ef9f6356da5d89bb7182c776417c6
Marek Hanzal <marek.hanzal@icloud.com>
2025-01-26 21:24:40 +0100

    Foo

--- apps/pico/src/app/derivean/game/GameMap2/Construction/Requirement/Item.tsx
+++ apps/pico/src/app/derivean/game/GameMap2/Construction/Requirement/Item.tsx
@@ -26,29 +26,15 @@ export const Item: FC<Item.Props> = ({ requirement }) => {
 				"p-2",
 				"cursor-default",
 				"hover:bg-slate-100",
-				requirement.supply > 0 && requirement.amount > available ?
-					[
-						"border-amber-400",
-						"bg-amber-50",
-						"hover:border-amber-600",
-						"hover:bg-amber-50",
-					]
-				:	undefined,
-				requirement.supply <= 0 && requirement.amount > available ?
-					[
-						"border-red-400",
-						"bg-red-50",
-						"hover:border-red-600",
-						"hover:bg-red-50",
-					]
-				:	undefined,
 			])}
 		>
 			<div className={"flex flex-row items-center justify-between"}>
-				{requirement.supply ?
-					<Icon icon={DemandIcon} />
-				:	<Icon icon={PackageIcon} />}
-				<div className={"font-bold"}>{requirement.name}</div>
+				<div className={"flex flex-row gap-2 items-center"}>
+					{requirement.supply ?
+						<Icon icon={DemandIcon} />
+					:	<Icon icon={PackageIcon} />}
+					<div className={"font-bold"}>{requirement.name}</div>
+				</div>
 
 				<div className={"flex flex-row gap-1 items-center"}>
 					<div>{toHumanNumber({ number: available })}</div>
