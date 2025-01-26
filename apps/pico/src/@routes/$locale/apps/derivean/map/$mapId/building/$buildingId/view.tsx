7feac2f58d0c85f785883902e3d25fc9c019d864
Marek Hanzal <marek.hanzal@icloud.com>
2025-01-26 17:57:38 +0100

    Building on waypoints

--- apps/pico/src/@routes/$locale/apps/derivean/map/$mapId/building/$buildingId/view.tsx
+++ apps/pico/src/@routes/$locale/apps/derivean/map/$mapId/building/$buildingId/view.tsx
@@ -4,6 +4,44 @@ import { BuildingPanel } from "~/app/derivean/game/GameMap2/Building/BuildingPan
 export const Route = createFileRoute(
 	"/$locale/apps/derivean/map/$mapId/building/$buildingId/view",
 )({
+	async loader({
+		context: { queryClient, kysely },
+		params: { mapId, buildingId },
+	}) {
+		/**
+		 * Find related buildings through waypoints.
+		 */
+		kysely.transaction().execute(async (tx) => {
+			const query = await tx
+				.withRecursive("ConnectedWaypoints", (qb) =>
+					qb
+						.selectFrom("Building_Waypoint as BW")
+						.where("BW.buildingId", "=", buildingId)
+						.select(["BW.waypointId"])
+						.unionAll(
+							qb
+								.selectFrom("Route as R")
+								.innerJoin(
+									"ConnectedWaypoints as CW",
+									"CW.waypointId",
+									"R.fromId",
+								)
+								.select(["R.toId as waypointId"]),
+						),
+				)
+				.selectFrom("ConnectedWaypoints as CW")
+				.innerJoin("Building_Waypoint as BW", "BW.waypointId", "CW.waypointId")
+				.innerJoin("Building as B", "B.id", "BW.buildingId")
+				.innerJoin("Land as l", "l.id", "B.landId")
+				.where("B.id", "!=", buildingId)
+				.where("l.mapId", "=", mapId)
+				.select(["B.id as buildingId"])
+				.distinct()
+				.execute();
+
+			console.log("bo", query);
+		});
+	},
 	component() {
 		const { building } = useLoaderData({
 			from: "/$locale/apps/derivean/map/$mapId/building/$buildingId",
