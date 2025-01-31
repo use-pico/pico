03f152d8531f98f0eb89378c8730649d31642c0c
Marek Hanzal <marek.hanzal@icloud.com>
2025-01-31 13:29:48 +0100

    Added route lengths

--- apps/pico/src/app/derivean/service/withBuildingGraph.ts
+++ apps/pico/src/app/derivean/service/withBuildingGraph.ts
@@ -4,10 +4,13 @@ import type { WithTransaction } from "~/app/derivean/db/WithTransaction";
 export namespace withBuildingGraph {
 	export interface Node {
 		type: "building" | "waypoint" | "route";
+		x: number;
+		y: number;
 	}
 
 	export interface Edge {
 		type: "route" | "building-waypoint";
+		length: number;
 		weight: number;
 	}
 
@@ -28,20 +31,29 @@ export const withBuildingGraph = async ({
 	const buildings = await tx
 		.selectFrom("Building as b")
 		.innerJoin("Land as l", "l.id", "b.landId")
-		.select(["b.id"])
+		.select(["b.id", "b.x", "b.y"])
 		.where("b.userId", "=", userId)
 		.where("l.mapId", "=", mapId)
 		.execute();
 	const waypoints = await tx
 		.selectFrom("Waypoint as w")
-		.select("w.id")
+		.select(["w.id", "w.x", "w.y"])
 		.where("w.mapId", "=", mapId)
 		.where("w.userId", "=", userId)
 		.execute();
 
 	const buildingWaypoints = await tx
 		.selectFrom("Building_Waypoint as bw")
-		.select(["bw.buildingId", "bw.waypointId"])
+		.innerJoin("Building as b", "b.id", "bw.buildingId")
+		.innerJoin("Waypoint as w", "w.id", "bw.waypointId")
+		.select([
+			"bw.buildingId",
+			"bw.waypointId",
+			"b.x as buildingX",
+			"b.y as buildingY",
+			"w.x as waypointX",
+			"w.y as waypointY",
+		])
 		.where(
 			"bw.buildingId",
 			"in",
@@ -55,7 +67,16 @@ export const withBuildingGraph = async ({
 		.execute();
 	const routes = await tx
 		.selectFrom("Route as r")
-		.select(["r.fromId", "r.toId"])
+		.innerJoin("Waypoint as from", "from.id", "r.fromId")
+		.innerJoin("Waypoint as to", "to.id", "r.toId")
+		.select([
+			"r.fromId",
+			"r.toId",
+			"from.x as fromX",
+			"from.y as fromY",
+			"to.x as toX",
+			"to.y as toY",
+		])
 		.where("r.userId", "=", userId)
 		.where("r.mapId", "=", mapId)
 		.execute();
@@ -66,26 +87,50 @@ export const withBuildingGraph = async ({
 		type: "undirected",
 	});
 
-	buildings.forEach(({ id }) =>
+	buildings.forEach(({ id, x, y }) =>
 		graph.addNode(id, {
 			type: "building",
+			x,
+			y,
 		}),
 	);
-	waypoints.forEach(({ id }) => {
+	waypoints.forEach(({ id, x, y }) => {
 		graph.addNode(id, {
 			type: "waypoint",
+			x,
+			y,
 		});
 	});
-	buildingWaypoints.forEach(({ buildingId, waypointId }) => {
-		graph.addEdge(buildingId, waypointId, {
-			type: "building-waypoint",
-			weight: 100,
-		});
-	});
-	routes.forEach(({ fromId, toId }) => {
+	buildingWaypoints.forEach(
+		({
+			buildingId,
+			waypointId,
+			buildingX,
+			buildingY,
+			waypointX,
+			waypointY,
+		}) => {
+			const length = Math.sqrt(
+				(waypointX - buildingX) ** 2 + (waypointY - buildingY) ** 2,
+			);
+
+			graph.addEdge(buildingId, waypointId, {
+				type: "building-waypoint",
+				length,
+				weight: length * 10,
+			});
+		},
+	);
+	routes.forEach(({ fromId, toId, fromX, fromY, toX, toY }) => {
+		const length = Math.sqrt((toX - fromX) ** 2 + (toY - fromY) ** 2);
+
 		graph.addEdge(fromId, toId, {
 			type: "route",
-			weight: 0,
+			length,
+			/**
+			 * Lower values has higher priority
+			 */
+			weight: length * -10,
 		});
 	});
 
