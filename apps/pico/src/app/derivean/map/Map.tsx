36f0c8ac3faafa6d3fa45b6a751d56d6999a6a30
Marek Hanzal <marek.hanzal@icloud.com>
2025-02-04 23:05:47 +0100

    Experiment

--- apps/pico/src/app/derivean/map/Map.tsx
+++ apps/pico/src/app/derivean/map/Map.tsx
@@ -1,5 +1,5 @@
 import { tvc } from "@use-pico/common";
-import { FC } from "react";
+import { FC, useRef, useState } from "react";
 import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
 import { Game } from "~/app/derivean/Game";
 
@@ -25,40 +25,40 @@ export namespace Map {
 export const Map: FC<Map.Props> = ({ mapId, land }) => {
 	const { size } = Game.world;
 
-	// const mapRef = useRef<HTMLDivElement>(null);
-	// const containerRef = useRef<HTMLDivElement>(null);
+	const mapRef = useRef<HTMLDivElement>(null);
+	const containerRef = useRef<HTMLDivElement>(null);
 
-	// const [landIds, setLandIds] = useState<string[]>([]);
+	const [landIds, setLandIds] = useState<string[]>([]);
 
-	// const updateVisibleLands = () => {
-	// 	if (!containerRef.current || !mapRef.current) {
-	// 		return;
-	// 	}
+	const updateVisibleLands = () => {
+		if (!containerRef.current || !mapRef.current) {
+			return;
+		}
 
-	// 	const container = containerRef.current.getBoundingClientRect();
-	// 	const map = mapRef.current.getBoundingClientRect();
+		const container = containerRef.current.getBoundingClientRect();
+		const map = mapRef.current.getBoundingClientRect();
 
-	// 	const scaleFactor = scale;
+		const scaleFactor = scale;
 
-	// 	const viewportLeft = (container.left - map.left) / scaleFactor;
-	// 	const viewportTop = (container.top - map.top) / scaleFactor;
-	// 	const viewportRight = viewportLeft + container.width / scaleFactor;
-	// 	const viewportBottom = viewportTop + container.height / scaleFactor;
+		const viewportLeft = (container.left - map.left) / scaleFactor;
+		const viewportTop = (container.top - map.top) / scaleFactor;
+		const viewportRight = viewportLeft + container.width / scaleFactor;
+		const viewportBottom = viewportTop + container.height / scaleFactor;
 
-	// 	const landsInView = land.filter((land) => {
-	// 		const x = (land.position % Game.world.lands) * Game.land.size;
-	// 		const y = Math.floor(land.position / Game.world.lands) * Game.land.size;
+		const landsInView = land.filter((land) => {
+			const x = (land.position % Game.world.lands) * Game.land.size;
+			const y = Math.floor(land.position / Game.world.lands) * Game.land.size;
 
-	// 		return (
-	// 			x + Game.land.size >= viewportLeft &&
-	// 			x <= viewportRight &&
-	// 			y + Game.land.size >= viewportTop &&
-	// 			y <= viewportBottom
-	// 		);
-	// 	});
+			return (
+				x + Game.land.size >= viewportLeft &&
+				x <= viewportRight &&
+				y + Game.land.size >= viewportTop &&
+				y <= viewportBottom
+			);
+		});
 
-	// 	setLandIds(landsInView.map((l) => l.id));
-	// };
+		setLandIds(landsInView.map((l) => l.id));
+	};
 
 	// useEffect(() => {
 	// 	updateVisibleLands();
@@ -79,35 +79,42 @@ export const Map: FC<Map.Props> = ({ mapId, land }) => {
 			minScale={0.1}
 			limitToBounds
 			disablePadding
+			onTransformed={(e) => {
+				console.log(e);
+			}}
 		>
-			<TransformComponent
-				wrapperClass={tvc(["w-screen", "h-screen", "overflow-hidden"])}
-			>
-				<div
-					className={tvc(["grid", "grid-cols-24"])}
-					style={{ width: size, height: size }}
-				>
-					{land.map((land) => {
-						return (
-							<div
-								key={`land-${land.id}`}
-								className={tvc([
-									"border-2",
-									"bg-purple-50",
-									"border-purple-600",
-									// landIds.includes(land.id) ?
-									// 	["bg-green-500"]
-									// :	["bg-purple-500"],
-								])}
-								style={{
-									width: Game.land.size,
-									height: Game.land.size,
-								}}
-							/>
-						);
-					})}
-				</div>
-			</TransformComponent>
+			{({ instance }) => {
+				return (
+					<TransformComponent
+						wrapperClass={tvc(["w-screen", "h-screen", "overflow-hidden"])}
+					>
+						<div
+							className={tvc(["grid", "grid-cols-24"])}
+							style={{ width: size, height: size }}
+						>
+							{land.map((land) => {
+								return (
+									<div
+										key={`land-${land.id}`}
+										className={tvc([
+											"border-2",
+											"bg-purple-50",
+											"border-purple-600",
+											// landIds.includes(land.id) ?
+											// 	["bg-green-500"]
+											// :	["bg-purple-500"],
+										])}
+										style={{
+											width: Game.land.size,
+											height: Game.land.size,
+										}}
+									/>
+								);
+							})}
+						</div>
+					</TransformComponent>
+				);
+			}}
 		</TransformWrapper>
 	);
 };
