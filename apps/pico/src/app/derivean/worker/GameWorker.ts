4b2668d78f8a973ed0c9cb15e19084d4e60cbe32
Marek Hanzal <marek.hanzal@icloud.com>
2025-02-18 22:56:18 +0100

    Added cache hits

--- apps/pico/src/app/derivean/worker/GameWorker.ts
+++ apps/pico/src/app/derivean/worker/GameWorker.ts
@@ -11,6 +11,12 @@ import { withColorMap } from "~/app/derivean/service/generator/withColorMap";
 import { withGenerator } from "~/app/derivean/service/generator/withGenerator";
 import type { Texture } from "~/app/derivean/Texture";
 
+/**
+ * Complicated, because of async functions.
+ */
+const chunkHits = new Int32Array(new SharedArrayBuffer(4));
+const textureHits = new Int32Array(new SharedArrayBuffer(4));
+
 const chunks = async (
 	id: string,
 	seed: string,
@@ -25,6 +31,8 @@ const chunks = async (
 	timer.start();
 
 	console.log(`Generating ${count} chunks, ${hash}`);
+
+	Atomics.store(chunkHits, 0, 0);
 
 	const generator = withGenerator({
 		plotCount: Game.plotCount,
@@ -51,8 +59,6 @@ const chunks = async (
 
 	const chunks = new Array<Promise<Chunks.Chunk>>(count);
 
-	const hit = 0;
-
 	try {
 		let index = 0;
 		for (let x = minX; x < maxX; x++) {
@@ -62,12 +68,16 @@ const chunks = async (
 					const chunkFile = `/chunk/${id}/${chunkId}.borsh`;
 
 					try {
-						return deserialize(
+						const data = deserialize(
 							ChunkBorshSchema,
 							decompressSync(
 								new Uint8Array(await file(chunkFile).arrayBuffer()),
 							),
 						) as Chunks.Chunk;
+
+						Atomics.add(chunkHits, 0, 1);
+
+						return data;
 					} catch (_) {
 						//
 					}
@@ -82,9 +92,7 @@ const chunks = async (
 					write(
 						chunkFile,
 						deflateSync(serialize(ChunkBorshSchema, data), { level: 9 }),
-					).then(() => {
-						console.log("Written chunk", chunkFile);
-					});
+					);
 
 					return data;
 				})();
@@ -101,7 +109,7 @@ const chunks = async (
 		})
 		.then((data) => {
 			console.log(
-				`\t- Chunks finished [hit ${hit}/${count}] [${timer.format()}]`,
+				`\t- Chunks finished [hit ${Atomics.load(chunkHits, 0)}/${count}] [${timer.format()}]`,
 			);
 			return data as Chunks.Chunk[];
 		});
@@ -132,7 +140,7 @@ export const textures = async (
 
 	const transfers: ArrayBufferLike[] = [];
 
-	let hit = 0;
+	Atomics.store(textureHits, 0, 0);
 
 	for (const chunk of chunks) {
 		const textureFile = `/texture/${id}/${chunk.id}.bin`;
@@ -150,7 +158,8 @@ export const textures = async (
 			};
 			transfers.push(image.data.buffer);
 
-			hit++;
+			Atomics.add(textureHits, 0, 1);
+
 			continue;
 		}
 
@@ -180,7 +189,7 @@ export const textures = async (
 	}
 
 	console.log(
-		`\t- Textures finished [hit ${hit}/${chunks.length}] [${timer.format()}]`,
+		`\t- Textures finished [hit ${Atomics.load(textureHits, 0)}/${chunks.length}] [${timer.format()}]`,
 	);
 
 	return transfer(textures, transfers);
