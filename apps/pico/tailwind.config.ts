d9ca905473244f26ad207ef273723bb35fcb7f37
Marek Hanzal <marek.hanzal@icloud.com>
2025-02-22 15:22:28 +0100

    There is some deep shit in vite dev/prod build

--- apps/pico/tailwind.config.ts
+++ apps/pico/tailwind.config.ts
@@ -1,6 +1,10 @@
 import { addDynamicIconSelectors } from "@iconify/tailwind";
 
 export default {
+	content: [
+		"../../node_modules/@use-pico/**/*.{ts,tsx}",
+		"../../packages/**/*.{ts,tsx}",
+	],
 	important: true,
 	plugins: [addDynamicIconSelectors()],
 };
--- apps/pico/vite.config.ts
+++ apps/pico/vite.config.ts
@@ -46,6 +46,6 @@ export default defineConfig({
 		// sourcemap: Boolean(process.env.TAURI_ENV_DEBUG),
 	},
 	optimizeDeps: {
-		exclude: ["sqlocal", "lru-cache"],
+		exclude: ["sqlocal"],
 	},
 });
