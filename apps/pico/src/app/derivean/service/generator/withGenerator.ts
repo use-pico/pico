e42a8fab59b3a7c00c669aa931831ba908c56a55
Marek Hanzal <marek.hanzal@icloud.com>
2025-02-24 17:12:20 +0100

    More comments

--- apps/pico/src/app/derivean/service/generator/withGenerator.ts
+++ apps/pico/src/app/derivean/service/generator/withGenerator.ts
@@ -69,25 +69,52 @@ export const withGenerator = ({
 	level,
 	noise,
 }: withGenerator.Props): withGenerator.Generator => {
+	/**
+	 * Define base scale for all noises.
+	 */
 	const baseScale = 1 / (gameConfig.plotCount * (1 / level.layer.level));
 
+	/**
+	 * Factory for creating individual noise generators.
+	 */
 	const { heightmap, biome, temperature, moisture } = noise({
 		seed,
 	});
 
+	/**
+	 * Returns prepared generator for generating chunk data at given position.
+	 */
 	return ({ x, z }) => {
+		/**
+		 * Defines overall chunk size (whole area).
+		 */
 		const size = gameConfig.plotCount ** 2;
+		/**
+		 * Texture buffer (RGBA) used to store chunk's texture.
+		 */
 		const buffer = new Uint8Array(size * 4);
 
+		/**
+		 * Go over all tiles in the chunk and generate their color based on various noises.
+		 */
 		for (let i = 0; i < size; i++) {
+			/**
+			 * Where a tile lies in the chunk coordinates.
+			 */
 			const tileX = i % gameConfig.plotCount;
 			const tileZ = Math.floor(i / gameConfig.plotCount);
+			/**
+			 * Compute world-wide chunk coordinates, so noise can be properly generated.
+			 */
 			const worldX =
 				(x * gameConfig.plotCount + (i % gameConfig.plotCount)) * baseScale;
 			const worldZ =
 				(z * gameConfig.plotCount + Math.floor(i / gameConfig.plotCount)) *
 				baseScale;
 
+			/**
+			 * Generate all noise values, used to construct a tile color (and other information).
+			 */
 			const [heightmapNoise, biomeNoise, temperatureNoise, moistureNoise] = [
 				heightmap(worldX, worldZ),
 				biome(worldX, worldZ),
@@ -95,17 +122,27 @@ export const withGenerator = ({
 				moisture(worldX, worldZ),
 			];
 
-			const inverse = gameConfig.plotCount - 1 - tileZ;
-
+			/**
+			 * Output the RGBA color to the final texture.
+			 */
 			buffer.set(
 				withColorMap({ value: heightmapNoise, gameConfig }),
-				(inverse * gameConfig.plotCount + tileX) * 4,
+				((gameConfig.plotCount - 1 - tileZ) * gameConfig.plotCount + tileX) * 4,
 			);
 		}
 
+		/**
+		 * Define chunk size based on a current level (LOD).
+		 */
 		const chunkSize = gameConfig.chunkSize * level.layer.level;
+		/**
+		 * Keep chunks on different levels properly aligned using this offset.
+		 */
 		const offset = (gameConfig.chunkSize * (level.layer.level - 1)) / 2;
 
+		/**
+		 * So, we're done here, currently only texture is returned.
+		 */
 		return {
 			id: `${x}:${z}:${level.layer.level}`,
 			x: x * chunkSize + offset,
