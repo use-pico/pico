93d00e943a3c6b981afd03a4410cc84c2290769c
Marek Hanzal <marek.hanzal@icloud.com>
2025-10-22 19:54:36 +0200

    chore: update package versions for @use-pico/client to 5.0.213, @use-pico/common to 5.0.90, and @use-pico/server to 5.0.94; refactor toTimeDiff utility for improved time difference calculations

--- packages/common/src/time/toTimeDiff.ts
+++ packages/common/src/time/toTimeDiff.ts
@@ -1,7 +1,7 @@
-import { DateTime, Duration, type ToHumanDurationOptions } from "luxon";
+import { DateTime } from "luxon";
 
 export namespace toTimeDiff {
-	export interface Props extends ToHumanDurationOptions {
+	export interface Props {
 		time: string;
 		/**
 		 * ISO date time
@@ -10,45 +10,17 @@ export namespace toTimeDiff {
 	}
 }
 
-export const toTimeDiff = ({
-	time,
-	source,
-	...options
-}: toTimeDiff.Props): string => {
+export const toTimeDiff = ({ time, source }: toTimeDiff.Props): string => {
 	const now = source ? DateTime.fromISO(source) : DateTime.now();
 	const target = DateTime.fromISO(time);
 
-	if (!target.isValid) {
+	if (!target.isValid) { 
+		console.warn("Invalid target", time);
 		return "0 min";
 	}
-	if (target <= now) {
-		return "0 min";
-	}
 
-	const diff = target
-		.diff(now, [
-			"weeks",
-			"days",
-			"hours",
-			"minutes",
-			"seconds",
-		])
-		.shiftTo("weeks", "days", "hours", "minutes");
-
-	const { weeks = 0, days = 0, hours = 0, minutes = 0 } = diff.toObject();
-
-	const duration = Duration.fromObject({
-		weeks: Math.floor(weeks),
-		days: Math.floor(days),
-		hours: Math.floor(hours),
-		minutes: Math.floor(minutes),
-	}).normalize();
-
-	return duration.toHuman({
-		unitDisplay: "short",
-		listStyle: "short",
-		maximumFractionDigits: 0,
-		showZeros: false,
-		...options,
+	return target.toRelative({
+		base: now,
+		style: "long",
 	});
 };
